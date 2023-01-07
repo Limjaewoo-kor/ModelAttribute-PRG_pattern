7aa896ff48e27555c9ec61d5249ae273fe9e19f2
Limjaewoo <ljw3944@naver.com>
2023-01-07 12:23:56 +0900

    spring

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.Locale;
 
 @Controller
 @RequestMapping("/basic/items")
@@ -19,11 +21,90 @@ public class BasicItemController {
 
     private final ItemRepository itemRepository;
 
-    @GetMapping()
+    @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items",items);
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String itme(@PathVariable long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model
+                       ){
+        Item item = new Item(itemName,price,quantity);
+        itemRepository.save(item);
+
+        model.addAttribute("item",item);
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model){
+        itemRepository.save(item);
+//        model.addAttribute("item",item);  @ModelAttribute에서 정한 이름으로 addAttribute까지 자동으로 해줌 생략가능.
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item){
+        //@ModelAttribute 뒤에 ()안에 값을 적지 않을경우 선언한 객체 (ex)Item에서 첫글자만 소문자로 바꾼 형태로 있는것으로 본다 - 결국 V2와 같다)
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV4(Item item){  //임의의 객체의 경우 @ModelAttribute 생략가능
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV5(Item item){
+        itemRepository.save(item);
+//        return "basic/item";  //PRG패턴(Post-Redirect-Get패턴)  - 저장하는 메소드의 경우 마지막에 리다이렉트를 시키지않으면 새로고침시 계속 저장된다.
+        return "redirect:/basic/items/"+item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item , RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+
+        //return Url에 +item.getId() 파라미터값을 바로 넣는것은 공백이나 한글등의 값일때 문제를 일으킬수있음.
+//        return "redirect:/basic/items/"+item.getId();
+        redirectAttributes.addAttribute("itemId",savedItem.getId());
+        redirectAttributes.addAttribute("status",true);
+        // status는 쿼리 파라미터로 붙게된다 ex) http://localhost:8080/basic/items/4?status=true
+        return "redirect:/basic/items/{itemId}";
+    }
+
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId,item);
+
+        return "redirect:/basic/items/{itemId}";
     }
 
 
@@ -33,7 +114,7 @@ public class BasicItemController {
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("itemA",10000,10));
-//        itemRepository.save(new Item("itemB",20000,20));
-//        itemRepository.save(new Item("itemC",30000,30));
+        itemRepository.save(new Item("itemB",20000,20));
+        itemRepository.save(new Item("itemC",30000,30));
     }
 }
